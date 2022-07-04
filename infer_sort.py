a102b854551ae65d6662e80a5ddf67ef343ed936
Dandan <daniel.leongsj@gmail.com>
2022-07-04 17:22:50 +0800

    add duration calculator

--- infer_sort.py
+++ infer_sort.py
@@ -1,4 +1,5 @@
 import os
+import librosa
 from typing import List, Union
 from inference import load_model, infer
 
@@ -25,6 +26,25 @@ def get_audiopaths(dirname: str) -> List[str]:
 
     return audiopaths
 
+def calculate_duration(audiopaths: List[str], output_dir: str, languages: List[str]) -> None:
+    os.makedirs(output_dir, exist_ok=True)
+    
+    duration_dict = {'en': 0, 'others': 0}
+
+    with open(os.path.join(output_dir, 'prediction_labels.csv'), 'w', encoding='utf-8') as fw:
+        fw.write('filename,label\n')
+    
+        for filepath, label in zip(audiopaths, languages):
+            if label == 'en: English':
+                duration_dict['en'] += librosa.get_duration(filename=filepath)
+            else:
+                duration_dict['others'] += librosa.get_duration(filename=filepath)
+            fw.write(f'{filepath},{label}\n')
+            
+    for key in duration_dict.keys():
+        duration = duration_dict[key] / 3600
+        print(f'Total for {key} (hrs): {round(duration, 3)}')
+
 def sort_audiopaths(audiopaths: List[str], dirname: str, languages: List[str]) -> None:
     # audiopaths input -> ['/workspace/data/xx/yy.wav', ...]
     # languages input -> ['id: Indonesian', 'id: Indonesian, 'ms: Malay', ...]
@@ -36,3 +56,4 @@ if __name__ == '__main__':
     audiopaths = get_audiopaths(SOURCE_MAIN_DIR)
     lang_predictions = infer(model, audiopaths)
     # sort_audiopaths(audiopaths, TARGET_MAIN_DIR, lang_predictions)
+    calculate_duration(audiopaths, output_dir='./outputs', languages=lang_predictions)
